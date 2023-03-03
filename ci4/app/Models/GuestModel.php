eed5a90a624d529f59b125e7042d01abe72c0f1b
Your Name <youremail@university.edu>
2023-03-03 16:08:52 +0800

    13

--- ci4/app/Models/GuestModel.php
+++ ci4/app/Models/GuestModel.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Models;
-
 use CodeIgniter\Model;
-
 class GuestModel extends Model
 {
     protected $table = 'zafrancia_myguests';
 
     protected $allowedFields = ['name', 'email', 'website', 'comment', 'gender'];
+
+
 	 public function getGuest()
     {     
         return $this->findAll();
