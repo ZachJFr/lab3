c331f4dae81766066ac7bba70225d129e6ef925f
Your Name <youremail@university.edu>
2023-03-03 15:47:36 +0800

    6

--- ci4/app/Controllers/Guest.php
+++ ci4/app/Controllers/Guest.php
@@ -19,25 +19,4 @@ class Guest extends BaseController
              . view('guest/index')
              . view('templates/footer');
     }
-    public function create ()
-    {
-        helper('form');
-
-        // Checks whether the form is submitted
-        if (! $this->request->is('post')) {
-            return view('templates/header', ['title', => 'Create a guest entry'])
-                . view('guest/create')
-                . view('templates/footer')
-        }
-
-        $post = $this->request->getPost(['name', 'email', 'website', 'comment', 'gender']);
-
-        if (! $this->validateData($post, [
-            'name' => 'required|max_length[255]|min_length[3]'
-            'email' => 'required|max_length[255]|min_length[3]'
-            'website' => 'required|max_length[255]|min_length[3]'
-            'comment' => 'required|max_length[5000]|min_length[10]'
-            'gender' => 'required|max_length[255]|min_length[3]'
-        ]))
-    }
 }
