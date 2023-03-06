59cc32158b61bedf8fc6179f88f59d7b749cf046
Your Name <youremail@university.edu>
2023-03-06 12:21:21 +0800

    minor changes 3

--- ci4/app/Views/guest/index.php
+++ ci4/app/Views/guest/index.php
@@ -1,25 +1,25 @@
-<h2><?= esc($title) ?></h2>
+<h2 style="color: white;"><?= esc($title) ?></h2>
 
 <?php if (! empty($guest) && is_array($guest)): ?>
 
     <?php foreach ($guest as $guest_item): ?>
 
-        <div class="main">
+        <div style="color: white;" class="main">
 		<br>
 		<b>
 		<table>
 		<tr>
-		<th>-----</th>
-		 <th><?= esc($guest_item['name']) ?> </th>
-         <th><?= esc($guest_item['email']) ?> </th>
-		 <th><?= esc($guest_item['comment']) ?> </th>
+		<th style="color: white;">-----</th>
+		 <th style="color: white;"><?= esc($guest_item['name']) ?> </th>
+         <th style="color: white;"><?= esc($guest_item['email']) ?> </th>
+		 <th style="color: white;"><?= esc($guest_item['comment']) ?> </th>
 		</tr>
 		</table>
         </div>
     <?php endforeach ?>
 
 <?php else: ?>
-    <h3>No guest</h3>
-    <p>Unable to find any guest for you.</p>
+    <h3 style="color: white;">No guest</h3>
+    <p style="color: white;">Unable to find any guest for you.</p>
 
 <?php endif ?>
