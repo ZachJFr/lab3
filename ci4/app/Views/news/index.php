839c1e1e7473867d68bc438e5fa562c211a2617f
Your Name <youremail@university.edu>
2023-03-06 12:24:06 +0800

    minor changes 5

--- ci4/app/Views/news/index.php
+++ ci4/app/Views/news/index.php
@@ -1,22 +1,22 @@
-<h2><?= esc($title) ?></h2>
+<h2 style="color: white;"><?= esc($title) ?></h2>
 
 <?php if (! empty($news) && is_array($news)): ?>
 
     <?php foreach ($news as $news_item): ?>
 
-        <h3><?= esc($news_item['title']) ?></h3>
+        <h3 style="color: white;"><?= esc($news_item['title']) ?></h3>
 
-        <div class="main">
+        <div style="color: white;" class="main">
             <?= esc($news_item['body']) ?>
         </div>
-        <p><a href="/lab3/ci4/public/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
+        <p style="color: white;"><a href="/lab3/ci4/public/news/<?= esc($news_item['slug'], 'url') ?>">View article</a></p>
 
     <?php endforeach ?>
 
 <?php else: ?>
 
-    <h3>No News</h3>
+    <h3 style="color: white;">No News</h3>
 
-    <p>Unable to find any news for you.</p>
+    <p style="color: white;">Unable to find any news for you.</p>
 
 <?php endif ?>
