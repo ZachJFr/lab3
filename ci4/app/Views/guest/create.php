b6a0177f21e2bde99d47fc43ae8cd96afe29cd54
Your Name <youremail@university.edu>
2023-03-06 12:09:26 +0800

    minor change

--- ci4/app/Views/guest/create.php
+++ ci4/app/Views/guest/create.php
@@ -6,25 +6,26 @@
 <form action="/~zafrancia/lab3/ci4/public/guest/create" method="post">
     <?= csrf_field() ?>
 
-    <label for="name">Name</label>
-    <input type="input" name="name" value="<?= set_value('name') ?>">
+    <label for="name" style="color: white;">Name</label>
+    <input style="color: white;"type="input" name="name" value="<?= set_value('name') ?>">
     <br>
 
-    <label for="email">Email</label>
-    <input type="input" name="email" value="<?= set_value('email') ?>">
+    <label for="email" style="color: white;">Email</label>
+    <input style="color: white;" type="input" name="email" value="<?= set_value('email') ?>">
     <br>
 
-    <label for="website">Website</label>
-    <input type="input" name="website" value="<?= set_value('website') ?>">
+    <label for="website" style="color: white;">Website</label>
+    <input style="color: white;" type="input" name="website" value="<?= set_value('website') ?>">
     <br>
 
-    <label for="comment">Comment</label>
-    <textarea name="comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
+    <label for="comment" style="color: white;">Comment</label>
+    <textarea style="color: white;" name="comment" cols="45" rows="4"><?= set_value('comment') ?></textarea>
     <br>
 
-    <label for="gender">Gender</label>
-    <input type="input" name="gender" value="<?= set_value('gender') ?>">
+    
+    <label for="gender" style="color: white;">Gender</label>
+    <input style="color: white;" type="input" name="gender" value="<?= set_value('gender') ?>">
     <br>
 
-    <input type="submit" name="submit" value="Create guest entry">
+    <input type="submit" name="submit" value="Create guest entry" style="color: white;">
 </form>
